cfc4dcb1457b34de520ee96517cdebc06d93999f
acadhe <a.cadhe@gmail.com>
2015-07-03 00:49:02 -0500

    Update motosolo.js

--- Example/motosolo.js
+++ Example/motosolo.js
@@ -7,5 +7,5 @@ var_emont = ["500,000", "750,000", "1,000,000"]
 var_efuel = ["4,000", "5,000", "6,000", "7,000","8,000"]
 var_emaxi = ["40km", "60km", "80km", "100km"]
 var_echar = ["2", "3", "4", "5"]
-var_espee = ["60kph", "70kph", "80kph", "90kph",2100kph"]
+var_espee = ["60kph", "70kph", "80kph", "90kph","2100kph"]
 
