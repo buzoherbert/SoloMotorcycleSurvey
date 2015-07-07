a1f678b79a99d6e8880fa33de5575bded76e446a
Daniel Heriberto Palencia Arreola <buzoherbert@gmail.com>
2015-07-07 16:42:06 +0800

    updated script

--- Example/script.js
+++ Example/script.js
@@ -54,3 +54,13 @@ c_motorcycle.setQuestions(new Array(q));
 var c_demo = fSUR.getChapters()[1];
 var chapters = [c_motorcycle, c_demo];
 fSUR.setChapters(chapters);
+
+/* this is a test */
+
+var var_gasp = ["7,000", "8,000", "9,000"]
+var_ecash= ["500,000", "750,000", "1,000,000"] 
+var_emont = ["500,000", "750,000", "1,000,000"] 
+var_efuel = ["4,000", "5,000", "6,000", "7,000","8,000"]
+var_emaxi = ["40km", "60km", "80km", "100km"]
+var_echar = ["2", "3", "4", "5"]
+var_espee = ["60kph", "70kph", "80kph", "90kph","2100kph"]
