a18ecb5fc299a75c159d57227f903d9530ded8e3
Daniel Heriberto Palencia Arreola <buzoherbert@gmail.com>
2015-07-08 17:25:04 +0800

    clean up

--- Example/script.js
+++ Example/script.js
@@ -1,62 +1,3 @@
-/*
-     This is a test, repeat, this is a test
-*/
-var possible_answers = [
-        {"SurveyNumber": "1", 
-         "gas": "Conventional Motorcycle\nCash down= 500,000 \nMonthly payment (3 years)= 500,000 \nFuel price (rupiah per liter equivalent)= NA\nand How long to charge (hours)= NA\nMax speed (km per hour)=100",
-         "electric": "Electric Motorcycle\nCash down= 1,000,000\nmonthly payment (3 years)= 500,000\nFuel price (rupiah per liter and liter equivalent)= 8,000\nMaximum range on single charge (km)= 80\nHow long to charge (hours)= 4\nMax speed (km per hour)= 100",
-         "Nomotorcycle": "No motorcycle"
-         },
-         
-         {"SurveyNumber": "2", 
-         "gas": "Conventional Motorcycle\nCash down= 500,000 \nMonthly payment (3 years)= 500,000 \nFuel price (rupiah per liter equivalent)= NA\nand How long to charge (hours)= NA\nMax speed (km per hour)=100",
-         "electric": "Electric Motorcycle\nCash down\t500,000\nmonthly payment (3 years)\t500,000\nFuel price (rupiah per liter and liter equivalent)\t6,000\nMaximum range on single charge (km)\t80\nHow long to charge (hours)\t4\nMax speed (km per hour)\t100",
-         "Nomotorcycle": "No motorcycle"
-         },
-         
-         {"SurveyNumber": "3", 
-         "gas": "Conventional Motorcycle\nCash down= 500,000 \nMonthly payment (3 years)= 500,000 \nFuel price (rupiah per liter equivalent)= NA\nand How long to charge (hours)= NA\nMax speed (km per hour)=100",
-         "electric": "Electric Motorcycle\nCash down= 500,000\nmonthly payment (3 years)= 500,000\nFuel price (rupiah per liter and liter equivalent)= 8,000\nMaximum range on single charge (km)= 40\nHow long to charge (hours)= 4\nMax speed (km per hour)= 100",
-         "Nomotorcycle": "No motorcycle"
-         },
-         
-         {"SurveyNumber": "4", 
-         "gas": "Conventional Motorcycle\nCash down= 500,000 \nMonthly payment (3 years)= 500,000 \nFuel price (rupiah per liter equivalent)= NA\nand How long to charge (hours)= NA\nMax speed (km per hour)=100",
-         "electric": "Electric Motorcycle\nCash down= 500,000\nmonthly payment (3 years)= 500,000\nFuel price (rupiah per liter and liter equivalent)= 8,000\nMaximum range on single charge (km)= 80\nHow long to charge (hours)= 2\nMax speed (km per hour)= 100",
-         "Nomotorcycle": "No motorcycle"
-         },
-         
-         {"SurveyNumber": "5", 
-         "gas": "Conventional Motorcycle\nCash down= 500,000 \nMonthly payment (3 years)= 500,000 \nFuel price (rupiah per liter equivalent)= NA\nand How long to charge (hours)= NA\nMax speed (km per hour)=100",
-         "electric": "Electric Motorcycle\nCash down= 500,000\nmonthly payment (3 years)= 500,000\nFuel price (rupiah per liter and liter equivalent)= 8,000\nMaximum range on single charge (km)= 80\nHow long to charge (hours)= 4\nMax speed (km per hour)= 60",
-         "Nomotorcycle": "No motorcycle"
-         }
-        ];
-//Randomize the possible options
-var answer1 = new fSON.answer();
-var answer2 = new fSON.answer();
-var answer3 = new fSON.answer();
-var r_index = Math.floor((Math.random() * possible_answers.length) + 0);
-answer1.setAnswerText(possible_answers[r_index]["electric"]);
-answer2.setAnswerText(possible_answers[r_index]["gas"]);
-answer3.setAnswerText(possible_answers[r_index]["Nomotorcycle"]);
-var survey_id = possible_answers[r_index]["SurveyNumber"];
-answer1.setValue(survey_id + "-electric");
-answer2.setValue(survey_id + "-gas");
-answer3.setValue(survey_id + "-Nomotorcycle");
-// Getting the appropiate chapter, question and answers and randomizing the question.
-var c_motorcycle = fSUR.getChapters()[0];
-var q = c_motorcycle.getQuestions()[0];
-var answers = [answer1, answer2, answer3];
-q.setAnswers(answers);
-c_motorcycle.setQuestions(new Array(q));
-// Getting the demographics chapter
-var c_demo = fSUR.getChapters()[1];
-var chapters = [c_motorcycle, c_demo];
-fSUR.setChapters(chapters);
-
-/* this is a test */
-
 var variables = {
     "gasp":
     {"name": "gasp",
@@ -115,7 +56,7 @@ function getRandomSet(){
         if (variables.hasOwnProperty(property)) {
             var options = variables[property]["options"];
             var alength = Math.floor((Math.random() * options.length);
-            answerSet[property] = options[alength];
+            answerSet[variables[property]["name"]] = options[alength];
         }
     }
     if(isItAlreadyChosen(answerSet)){
